8e10a7fbd797ccec525c96b54ee3e7172af87eb6
GalinManov <galin.manov00@gmail.com>
2023-03-01 23:30:08 +0200

    Add default game details page

--- src/components/CatalogItem.js
+++ src/components/CatalogItem.js
@@ -7,7 +7,7 @@ const CatalogItem = ({game}) => {
                 <img src={game.imageUrl} />
                 <h6>{game.category}</h6>
                 <h2>{game.title}</h2>
-                <Link to='/catalog:id' className="details-button">Details</Link>
+                <Link to={`/catalog/${game._id}`} className="details-button">Details</Link>
             </div>
         </div>
     )
